a836ef71045ff82f72e78cdaaa8b8cd7d6cb9571
khadiza9292 <khadizatul.qubra@chasacademy.se>
2023-10-05 20:55:56 +0200

    routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import{BrowserRouter,Routes, Route} from 'react-router-dom'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Profile from './pages/Profile'
+import Home from './pages/Home'
+import About from './pages/About'
 
 export default function App() {
   return (
-    <div className='text-2xl text-orange-700'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/about' element ={<About/>}/>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<SignIn/>} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='text-center text-yellow-800 top-10 text-8xl'>Home</div>
+  )
+}
