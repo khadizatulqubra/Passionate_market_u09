2a0240b6cec94f4d5b6e1d00f9a5490a5d65e07c
khadiza9292 <khadizatul.qubra@chasacademy.se>
2023-10-06 19:13:18 +0200

    Middleware created and error handle function created

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,8 @@
  import User from '../models/user.model.js';
  import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js';
 
- export const signup= async(req,res)=>{  
+ export const signup= async(req,res,next)=>{  
 
 
     const {username, email, password}=req.body;
@@ -11,8 +12,8 @@
         await newUser.save();
         res.status (201).json("User created successfully");
 
-    }catch (error){
-        res.status(409).json({message:error.message});
+    } catch (error){
+        next(error);
     }
  
 };
