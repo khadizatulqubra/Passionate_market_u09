a30c0c58085a53a31c11de95ae65d46e1f92e96f
khadiza9292 <khadizatul.qubra@chasacademy.se>
2023-10-06 11:59:21 +0200

    header component fixing

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import SignIn from './pages/SignIn'
 import Profile from './pages/Profile'
 import Home from './pages/Home'
 import About from './pages/About'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path='/about' element ={<About/>}/>
+        <Route path="/about"element ={<About/>}/>
         <Route path="/profile" element={<Profile />} />
         <Route path="/signin" element={<SignIn/>} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   )
+  
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='text-center text-yellow-800 top-10 text-8xl'>Home</div>
+    <div className='text-yellow-800 '>Home</div>
   )
 }
