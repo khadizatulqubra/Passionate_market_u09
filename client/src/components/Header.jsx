2b51cda9a9596ff34a416be0c108b2dca85259ef
khadiza9292 <khadizatul.qubra@chasacademy.se>
2023-10-06 17:26:07 +0200

    route testing

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,36 +4,36 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="bg-pink-600 shadow-md bg-opacity-10">
-      <div className="flex items-center justify-between max-w-6xl p-3 mx-auto">
+      <div className="flex items-center justify-between max-w-6xl p-3 mx-auto ">
         <Link to="/" >
-       <div className="top-0 flex items-center bg-pink-700 rounded-full h-28 w-28 drop-shadow-2xl "  >
+       <div className="top-0 flex items-center bg-pink-900 rounded-full bg-gradient-to-bl h-28 w-28 drop-shadow-2xl shadow-stone-400"  >
        <h1 className="flex flex-wrap items-center py-8 text-sm font-bold sm:text-xl">
-          <span className="items-center inline-block font-mono text-sm italic font-bold text-red-400 align-middle -rotate-12 m-0.5">Pas</span>
-          <span className="font-mono text-lg italic font-bold text-red-500 ">Sion</span>
-          <span className="font-mono text-sm italic font-bold text-red-400 -rotate-12">Nate</span>
-          <span className="ml-2 font-mono text-lg italic text-red-500 -rotate-12">Mar</span>
-          <span className="font-mono text-sm italic font-bold text-red-400 ">ket</span>
+          <span className="items-center inline-block font-mono text-sm italic font-bold text-red-400 align-middle -rotate-12 m-0.5 ">Pas</span>
+          <span className="font-mono text-lg italic font-bold text-red-500 rotate-12">Sion</span>
+          <span className="font-mono text-sm italic font-bold text-red-400 rotate-12">Nate</span>
+          <span className="ml-4 font-mono text-lg italic text-red-500 -rotate-12">Mar</span>
+          <span className="font-mono text-lg italic font-bold text-red-400 rotate-12">ket</span>
         </h1>
        </div>
         </Link>
-        <form action="" className="flex items-center p-3 bg-red-300 rounded-lg bg-opacity-60">
+        <form action="" className="flex items-center p-3 bg-red-200 rounded-lg ">
           <input
             type="text"
             placeholder="Search...."
-            className="w-2 bg-red-100 rounded-lg focus:outline-none sm:w-64 text-slate-800 bg-opacity-60"
+            className="w-2 bg-red-200 rounded-lg focus:outline-none sm:w-64 text-slate-800 "
           />
           <FaSearch className="text-slate-6000" />
         </form>
         <ul className="flex gap-4">
             
         <Link to='/home'>
-        <li className="hidden text-red-400 sm:inline hover:underline">Home</li>
+        <li className="hidden text-pink-900 sm:inline hover:underline">Home</li>
         </Link>
         <Link to='/about'>
-          <li className="hidden text-red-400 sm:inline hover:underline"> About</li>
+          <li className="hidden text-pink-900 sm:inline hover:underline"> About</li>
           </Link>
           <Link to='/singin'>
-          <li className="text-red-400 sm:inline hover:underline">SignIn </li>
+          <li className="text-pink-900 sm:inline hover:underline">SignIn </li>
           </Link>
         </ul>
       </div>
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+
+import userRouter from './routes/user.route.js'
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI).then(() =>{
@@ -15,3 +17,6 @@ const app =express();
 app.listen (3000,()=>{
     console.log('server is running on port 3000');
 })
+
+// Path: server/index.js
+app.use("/api/user" ,userRouter)
