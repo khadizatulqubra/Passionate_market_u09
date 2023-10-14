7f30c42f3f531af0903f8db7de5b9c4e88418629
khadiza9292 <khadizatul.qubra@chasacademy.se>
2023-10-14 22:41:03 +0200

    cteate listing api created

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import listingRouter from './routes/listing.route.js';
 
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
@@ -25,6 +26,7 @@ app.listen (3000,()=>{
 // Path: server/index.js
 app.use("/api/user" ,userRouter)
 app.use ( "/api/auth",  authRouter)
+app.use ( "/api/listing",  listingRouter)
 
 app.use(( err,req,res,next)=>{
     const statusCode=err.statusCode||500;
