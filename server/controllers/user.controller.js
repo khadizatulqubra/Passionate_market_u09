11487b6c0cc1aa7c611ef5d1806b4fa7613daa24
khadiza9292 <khadizatul.qubra@chasacademy.se>
2023-10-22 11:09:50 +0200

    get user listings api route created

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,6 +2,7 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js'
 import {errorHandler} from '../utils/error.js';
+import Listing from '../models/listing.model.js';
 
 export const test = (req,res)=>{
     res.json({
@@ -50,4 +51,32 @@ res.status(200).json('User has been deleted');
         next (error)
 
 }
-}
+};
+
+export const getUserListings = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
+      try {
+        const listings = await Listing.find({ userRef: req.params.id });
+        res.status(200).json(listings);
+      } catch (error) {
+        next(error);
+      }
+    } else {
+      return next(errorHandler(401, 'You can only view your own listings!'));
+    }
+  };
+  
+  export const getUser = async (req, res, next) => {
+    try {
+      
+      const user = await User.findById(req.params.id);
+    
+      if (!user) return next(errorHandler(404, 'User not found!'));
+    
+      const { password: pass, ...rest } = user._doc;
+    
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
+    }
+  };
