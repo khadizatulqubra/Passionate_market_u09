767512a28c286ec3a23afb4a0f9a2741a97d413e
khadiza9292 <khadizatul.qubra@chasacademy.se>
2023-10-25 20:24:44 +0200

    listing page complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
 import UpdateListing from './pages/UpdateListing'
+import Listing from './pages/Listing'
 
 export default function App() {
   return (
@@ -24,6 +25,7 @@ export default function App() {
         </Route>
         <Route path="/signin" element={<SignIn/>} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path='/listing/:listingId' element={<Listing />} />
         <Route
             path='/update-listing/:listingId'
             element={<UpdateListing />}
