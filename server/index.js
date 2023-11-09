88634360a024e283e45e95bfec6c5d90c5e9e168
khadiza9292 <khadizatul.qubra@chasacademy.se>
2023-11-09 19:10:36 +0100

    add pre instalation done

--- server/index.js
+++ server/index.js
@@ -5,7 +5,8 @@ import listingRouter from './routes/listing.route.js';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
-// import path from 'path';
+
+ import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI).then(() =>{
@@ -18,7 +19,7 @@ mongoose.connect(process.env.MONGODB_URI).then(() =>{
 })
 
 
-
+const __dirname=path.resolve();
 
 
 const app =express();
@@ -35,12 +36,12 @@ app.use ( "/api/auth",  authRouter)
 app.use ( "/api/listing",  listingRouter)
 
 
-/* app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
   })
- */
+ 
 app.use(( err,req,res,next)=>{
     const statusCode=err.statusCode||500;
     const message=err.message||'Internal Server Error Hapened';
